8cd02189b97ddd18a8f5ffae3b94eb219bd57e73
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-10-28 22:30:36 +0100

    something

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
+<<<<<<< Updated upstream
 	return <main></main>;
+=======
+	return (
+		<main>
+			<h1>Hejsan</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus harum corporis porro eius molestias ea enim aut molestiae quas numquam.</p>
+		</main>
+	);
+>>>>>>> Stashed changes
 }
