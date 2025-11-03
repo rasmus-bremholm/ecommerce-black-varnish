870c8411b6f774ae7c6aa16dc67e01fc2a8f4d9b
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-03 22:20:25 +0100

    CategoryPage update

--- app/(shop)/products/[category]/page.tsx
+++ app/(shop)/products/[category]/page.tsx
@@ -1,10 +1,23 @@
 import { client } from "@/lib/sanity/client";
 import { urlFor } from "@/lib/sanity/image";
 import { groq } from "next-sanity";
+import { Box, Container, Typography } from "@mui/material";
 
 export default async function CategoryPage({ params }: { params: Promise<{ category: string }> }) {
-	const category = await params;
-	const products = await client.fetch(groq`*[_type == "product" && category->slug.current == $category]`, { category });
+	const { category: categorySlug } = await params;
 
-	return <main></main>;
+	const categoryData = await client.fetch(groq`*[_type == "category" && slug.current == $categorySlug][0]{name, description}`, { categorySlug });
+	const products = await client.fetch(groq`*[_type == "product" && category->slug.current == $categorySlug]`, { categorySlug });
+
+	return (
+		<Container maxWidth='lg'>
+			<Box>
+				<Typography variant='h2' component='h1'>
+					{categoryData.name}
+				</Typography>
+				<Typography>{categoryData.description}</Typography>
+			</Box>
+			<Box>{/* Products Grid or something */}</Box>
+		</Container>
+	);
 }
